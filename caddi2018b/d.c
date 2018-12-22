59428306c07134870d2a38198bf6c41651661803
Minagi Yu <39975793+minagi-yu@users.noreply.github.com>
2018-12-22 22:56:03 +0900

    caddi2018b_d_1wa

---
+++ caddi2018b/d.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (void)
+{
+    long long N, a;
+    int i, odd = 0;
+
+    scanf("%lld", &N);
+
+    for (i = 0; i < N; i++) {
+        scanf("%lld", &a);
+        if (a % 2 != 0) odd++;
+    }
+
+    if (odd % 2 == 0) {
+        puts("second");
+    } else {
+        puts("first");
+    }
+}
