6e38439f5daa13480946a93ff23b34892d1b9496
Minagi Yu <39975793+minagi-yu@users.noreply.github.com>
2018-12-29 21:10:57 +0900

    agc030_a_1wa

---
+++ agc030/a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    long long A, B, C;
+    long long ans = 0;
+
+    scanf("%lld %lld %lld", &A, &B, &C);
+
+    if (C > A + B) {
+        ans = A + B + 1;
+    } else if (C > A) {
+        ans = A + 1;
+    } else {
+        ans = C;
+    }
+
+    ans += B;
+
+    printf("%lld\n", ans);
+
+    return 0;
+}
